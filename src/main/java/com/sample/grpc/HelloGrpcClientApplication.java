d2b3dbb890af2224e591980e0ee8cdc08a87730d
sarav <sarav@demo.com>
2024-09-22 10:13:05 +0100

    Base Spring boot app

---
+++ src/main/java/com/sample/grpc/HelloGrpcClientApplication.java
@@ -0,0 +1,13 @@
+package com.sample.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGrpcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGrpcClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/grpc/HelloGrpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGrpcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
