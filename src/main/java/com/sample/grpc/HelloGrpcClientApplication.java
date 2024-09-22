4bbc9bcaaeaa1224c27d25718a4738750a13758e
sarav <sarav@demo.com>
2024-09-22 11:10:11 +0100

    Implemented call to service

--- src/main/java/com/sample/grpc/HelloGrpcClientApplication.java
+++ src/main/java/com/sample/grpc/HelloGrpcClientApplication.java
@@ -1,13 +1,21 @@
 package com.sample.grpc;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloGrpcClientApplication {
 
+	@Autowired
+	SimpleGrpcClient simpleGrpcClient;
 	public static void main(String[] args) {
 		SpringApplication.run(HelloGrpcClientApplication.class, args);
 	}
 
+	@PostConstruct
+	private void callGrpc() {
+		simpleGrpcClient.helloClient("gRPC");
+	}
 }
--- src/main/java/com/sample/grpc/SimpleGrpcClient.java
+++ src/main/java/com/sample/grpc/SimpleGrpcClient.java
@@ -1,10 +1,22 @@
 package com.sample.grpc;
 
+import com.sample.grpc.proto.HelloReply;
+import com.sample.grpc.proto.HelloRequest;
+import com.sample.grpc.proto.SimpleGrpc;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleGrpcClient {
-//    @GrpcClient
-//    private  Simple
+    @GrpcClient(value = "simpleService") //this is what was used in app props
+    private SimpleGrpc.SimpleBlockingStub simpleBlockingStub;
+
+    public void helloClient(String name){
+        HelloRequest helloRequest = HelloRequest.newBuilder().setName(name).build();
+
+        HelloReply helloReply = simpleBlockingStub.greeting(helloRequest);
+        System.out.println(helloReply.getMessage());
+
+    }
+
 }
