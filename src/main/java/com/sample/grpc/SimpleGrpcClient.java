302bd139ec421f2fcc90f4ed546855a957f88de1
sarav <sarav@demo.com>
2024-09-22 10:28:59 +0100

    Fix: added grpc spring boot starter, not server

---
+++ src/main/java/com/sample/grpc/SimpleGrpcClient.java
@@ -0,0 +1,10 @@
+package com.sample.grpc;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleGrpcClient {
+//    @GrpcClient
+//    private  Simple
+}
